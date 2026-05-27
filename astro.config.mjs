e3825d8c2ad12601cb349139f0f7800c5018a73a
TAITS88 <ruhantait@gmail.com>
2026-05-27 13:32:06 +0200

    Fix Astro Cloudflare server config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
-	integrations: [mdx(), sitemap()],
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: "https://whichwood.co.za",
+  output: "server",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  })
 });
